892bd979cd92a997af6e1a6d149ce99b81903210
Galen Helgemo <GalenHelgemo@gmail.com>
2020-01-01 21:05:04 -0600

    Initial Day 7 C# development.

---
+++ 2018/C#/Helpers/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Helpers
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class Helper
+    {
+        public static string ReadStringFromFile(string relativeFilePath) =>
+            new StreamReader(Directory.GetCurrentDirectory() + relativeFilePath).ReadToEnd();
+
+        public static List<string> ReadStringArrayFromFile(string relativeFilePath) =>
+            ReadStringFromFile(relativeFilePath).Split('\n', '\r').Where(x => x != string.Empty).ToList();
+    }
+}
